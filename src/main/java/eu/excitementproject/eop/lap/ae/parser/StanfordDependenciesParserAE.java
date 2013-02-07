77c85844fb156be45444d2da2cb1d866fe02a721
Ofer Bronstein <oferbr@gmail.com>
2013-02-07 15:51:17 +0200

    finish StanfordDependenciesParserAE (before testing)

--- src/main/java/eu/excitementproject/eop/lap/ae/parser/StanfordDependenciesParserAE.java
+++ src/main/java/eu/excitementproject/eop/lap/ae/parser/StanfordDependenciesParserAE.java
@@ -1,9 +1,13 @@
 package eu.excitementproject.eop.lap.ae.parser;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import org.apache.uima.UimaContext;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
@@ -17,10 +21,16 @@ import de.tudarmstadt.ukp.dkpro.core.api.lexmorph.type.pos.POS;
 import de.tudarmstadt.ukp.dkpro.core.api.resources.MappingProvider;
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Lemma;
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence;
+import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
+import de.tudarmstadt.ukp.dkpro.core.api.syntax.type.dependency.Dependency;
 import eu.excitementproject.eop.common.representation.parse.representation.basic.EdgeInfo;
 import eu.excitementproject.eop.common.representation.parse.representation.basic.NodeInfo;
+import eu.excitementproject.eop.common.representation.parse.tree.AbstractNodeUtils;
 import eu.excitementproject.eop.common.representation.parse.tree.dependency.basic.BasicConstructionNode;
 import eu.excitementproject.eop.common.representation.partofspeech.PennPartOfSpeech;
+import eu.excitementproject.eop.common.representation.partofspeech.UnsupportedPosTagStringException;
+import eu.excitementproject.eop.lap.ae.AbstractNodeCASUtils;
+import eu.excitementproject.eop.lap.ae.AbstractNodeCasException;
 import eu.excitementproject.eop.lap.ae.SingletonSynchronizedAnnotator;
 import eu.excitementproject.eop.lap.biu.en.parser.BasicPipelinedParser;
 import eu.excitementproject.eop.lap.biu.en.parser.ParserRunException;
@@ -38,144 +48,124 @@ import eu.excitementproject.eop.lap.biu.postagger.PosTaggedToken;
  */
 public abstract class StanfordDependenciesParserAE<T extends BasicPipelinedParser> extends SingletonSynchronizedAnnotator<T> {
 
-	protected MappingProvider mappingProvider;
-	
-	@Override
-	public void initialize(UimaContext aContext)
-		throws ResourceInitializationException
-	{
-		super.initialize(aContext);
+	private static final String DEPPACKAGE = Dependency.class.getPackage().getName()+".";
 
-		mappingProvider = new MappingProvider();
-		mappingProvider.setDefault(MappingProvider.BASE_TYPE, POS.class.getName());
-		configureMapping();
-	}
+	private static final Set<String> DEEP_DEPENDENCY_RELATIONS = new HashSet<String>(Arrays.asList(new String[] {
+			"xsubj",
+			"ref"
+	}));
 	
 	@Override
 	public void process(JCas jcas) throws AnalysisEngineProcessException {
 		try {
-			//CAS cas = jcas.getCas();
-			//mappingProvider.configure(cas);
 			
-			Collection<Sentence> sentenceAnnotations = JCasUtil.select(jcas, Sentence.class);
-			List<List<POS>> posAnnotations = new ArrayList<List<POS>>(sentenceAnnotations.size());
-			//List<List<String>> tokenStrings = new ArrayList<List<String>>(sentenceAnnotations.size());
+			for (Sentence sentenceAnno : JCasUtil.select(jcas, Sentence.class)) {
+				List<PosTaggedToken> taggedTokens = new ArrayList<PosTaggedToken>();
 
-			List<List<PosTaggedToken>> taggedTokens = new ArrayList<List<PosTaggedToken>>(sentenceAnnotations.size());
-
-			for (Sentence sentenceAnnotation : sentenceAnnotations) {
-				List<POS> posAnnotationsOnesentence = JCasUtil.selectCovered(jcas, POS.class, sentenceAnnotation);
-				posAnnotations.add(posAnnotationsOnesentence);
-				List<PosTaggedToken> taggedTokensOneSentence = new ArrayList<PosTaggedToken>(posAnnotationsOnesentence.size());
-				taggedTokens.add(taggedTokensOneSentence);
-				for (POS posAnnotation : posAnnotationsOnesentence) {
-					String tokenText = posAnnotation.getCoveredText();
-					PennPartOfSpeech partOfSpeech = new PennPartOfSpeech(posAnnotation.getPosValue());
+				// Make sure this list is an ArrayList, so that we can access elements by index freely (random access)
+				ArrayList<Token> tokenAnnotations = new ArrayList<Token>(JCasUtil.selectCovered(jcas, Token.class, sentenceAnno));
+			
+				// Build PosTaggedToken list of the sentence
+				for (Token tokenAnno : tokenAnnotations) {
+					String tokenText = tokenAnno.getCoveredText();
+					PennPartOfSpeech partOfSpeech = new PennPartOfSpeech(tokenAnno.getPos().getPosValue());
 					PosTaggedToken taggedToken = new PosTaggedToken(tokenText, partOfSpeech);
-					taggedTokensOneSentence.add(taggedToken);
-					
+					taggedTokens.add(taggedToken);
 				}
 				
-//				tokenAnnotations.add(tokenAnnotationsOnesentence);
-//				List<String> tokenStringsOneSentence = JCasUtil.toText(tokenAnnotationsOnesentence);
-//				tokenStrings.add(tokenStringsOneSentence);
-			}
-			
-			List<List<BasicConstructionNode>> noseLists = new ArrayList<List<BasicConstructionNode>>(sentenceAnnotations.size());
-
-			
-			// Using the inner tool
-			// This is done in a different for-loop, to avoid entering the synchornized() block
-			// many times (once per Sentence)
-			synchronized (innerTool) {
-				for (List<PosTaggedToken> taggedTokensOneSentence : taggedTokens) {
-					innerTool.setSentence(taggedTokensOneSentence);
+				BasicConstructionNode root;
+				ArrayList<BasicConstructionNode> orderedNodes;
+				
+				synchronized (innerTool) {
+					innerTool.setSentence(taggedTokens);
 					innerTool.parse();
-					List<BasicConstructionNode> nodeListOneSentence = innerTool.getNodesOrderedByWords();
-					noseLists.add(nodeListOneSentence);
+					root = innerTool.getMutableParseTree();
+					orderedNodes = innerTool.getNodesOrderedByWords();
 				}
-			}
-			
-			// Process each sentence
-			Iterator<List<POS>> iterPosAnnotations = posAnnotations.iterator();
-			Iterator<List<BasicConstructionNode>> iterNodeLists = noseLists.iterator();
-			while (iterPosAnnotations.hasNext()) {
-				List<POS> posAnnotationsOnesentence = iterPosAnnotations.next();
-				List<BasicConstructionNode> nodeListOneSentence = iterNodeLists.next();
 				
-				if (posAnnotationsOnesentence.size() != nodeListOneSentence.size()) {
-					throw new ParserRunException("Got parse for " + nodeListOneSentence.size() +
-							" tokens, should have gotten according to the total number of tokens in the sentence: " + posAnnotationsOnesentence.size());
+				if (tokenAnnotations.size() != orderedNodes.size()) {
+					throw new ParserRunException("Got parse for " + orderedNodes.size() +
+							" tokens, should have gotten according to the total number of tokens in the sentence: " + tokenAnnotations.size());
 				}
-								
-				// Process each token
-				Iterator<POS> iterPosAnnotationsOnesentence = posAnnotationsOnesentence.iterator();
-				Iterator<BasicConstructionNode> iterNodeListOneSentence = nodeListOneSentence.iterator();
-				while (iterPosAnnotationsOnesentence.hasNext()) {
-					POS posAnnotation = iterPosAnnotationsOnesentence.next();
-					//String tagString = taggedToken.getPartOfSpeech().getStringRepresentation();
-					BasicConstructionNode node = iterNodeListOneSentence.next();
+				
+				// Get extra nodes
+				Set<BasicConstructionNode> extraNodes = AbstractNodeUtils.treeToSet(root);
+				extraNodes.removeAll(orderedNodes);
+				
+				// Get parent map
+				Map<BasicConstructionNode, BasicConstructionNode> parentMap = AbstractNodeUtils.parentMap(root);
+				
+				// Handle all concrete (non-extra) nodes
+				for (BasicConstructionNode node : orderedNodes) {
 					NodeInfo nodeInfo = node.getInfo().getNodeInfo();
-					EdgeInfo edgeInfo = node.getInfo().getEdgeInfo();
-					
-					// validate word text
-					String tokenText = posAnnotation.getCoveredText();
-					if (tokenText != nodeInfo.getWord()) {
-						throw new ParserRunException("For token covering text \"" + tokenText + "\", got from parse a node " +
-									"with text \"" + nodeInfo.getWord() + "\" (serial=" + nodeInfo.getSerial() + ")");
-					}
+					Token tokenAnno = AbstractNodeCASUtils.nodeToToken(tokenAnnotations, node);
 					
 					// handle Lemma
-					Lemma lemma = new Lemma(jcas, token.getBegin(), token.getEnd());
+					Lemma lemma = new Lemma(jcas, tokenAnno.getBegin(), tokenAnno.getEnd());
 					lemma.setValue(nodeInfo.getWordLemma());
 					lemma.addToIndexes();
-					token.setLemma(lemma);
+					tokenAnno.setLemma(lemma);
 					
-					// handle Dependency
-					//////////////////////////////////////////////////////
-					// Taken from de.tudarmstadt.ukp.dkpro.core.stanfordnlp.util.StanfordAnnotator.createDependencyAnnotation()
-					String dependencyType = aGramRel.getShortName();
-
-					if (dependencyType.equalsIgnoreCase("AUX")) {
-						dependencyType = "AUX0";
+					// process dependency of concrete node
+					if (node != root) {
+						BasicConstructionNode parentNode = parentMap.get(node);
+						String relationName = node.getInfo().getEdgeInfo().getDependencyRelation().getStringRepresentation();
+						processDependency(jcas, tokenAnno, parentNode, relationName, tokenAnnotations, sentenceAnno);
 					}
-
-					// create the necessary objects and methods
-					String dependencyTypeName = DEPPACKAGE + dependencyType.toUpperCase();
-
-					Type type = jCas.getTypeSystem().getType(dependencyTypeName);
-			        if (type == null) {
-						throw new IllegalStateException("Type [" + dependencyTypeName + "] mapped to tag ["
-								+ dependencyType + "] is not defined in type system");
-			        }
+				}
+				
+				// Handle all extra nodes
+				for (BasicConstructionNode node : extraNodes) {
+					if (node.getAntecedent() == null) {
+						throw new ParserRunException("Got node that should hve an antecedent, but doesn't have one: " + node);
+					}
 					
-					AnnotationFS anno = jCas.getCas().createAnnotation(type, aBegin, aEnd);
-					anno.setStringValue(type.getFeatureByBaseName("DependencyType"), aGramRel.toString());
-					anno.setFeatureValue(type.getFeatureByBaseName("Governor"), aGovernor);
-					anno.setFeatureValue(type.getFeatureByBaseName("Dependent"), aDependent);
-
-					jCas.addFsToIndexes(anno);
-					//////////////////////////////////////////////////////////////
+					// the parent in the dependency is the topmost antecedent
+					BasicConstructionNode dependencyParentNode = AbstractNodeUtils.getDeepAntecedentOf(node);
 					
+					// the child in the dependency is the node's parent
+					BasicConstructionNode dependencyChildNode = parentMap.get(node);
+					Token dependencyChildToken = AbstractNodeCASUtils.nodeToToken(tokenAnnotations, dependencyChildNode);
 					
+					String relationName = node.getInfo().getEdgeInfo().getDependencyRelation().getStringRepresentation();
+					processDependency(jcas, dependencyChildToken, dependencyParentNode, relationName, tokenAnnotations, sentenceAnno);
 					
-					
-					// Get an annotation with the appropriate UIMA type via the mappingProvider
-					Type posTag = mappingProvider.getTagType(tagString);
-					POS posAnnotation = (POS) cas.createAnnotation(posTag, tokenAnnotation.getBegin(), tokenAnnotation.getEnd());
-					posAnnotation.setPosValue(tagString);
-					posAnnotation.addToIndexes();
-					
-					tokenAnnotation.setPos(posAnnotation);
 				}
 			}
 		} catch (ParserRunException e) {
 			throw new AnalysisEngineProcessException(AnalysisEngineProcessException.ANNOTATOR_EXCEPTION, null, e);
+		} catch (AbstractNodeCasException e) {
+			throw new AnalysisEngineProcessException(AnalysisEngineProcessException.ANNOTATOR_EXCEPTION, null, e);
+		} catch (UnsupportedPosTagStringException e) {
+			throw new AnalysisEngineProcessException(AnalysisEngineProcessException.ANNOTATOR_EXCEPTION, null, e);
 		}
 	}
 	
-	/**
-	 * Allow the subclass to provide details regarding its MappingProvider.
-	 */
-	protected abstract void configureMapping();
+	public static Set<String> getDeepDependencyRelations() {
+		return DEEP_DEPENDENCY_RELATIONS;
+	}
+	
+	private void processDependency(JCas jcas, Token childToken, BasicConstructionNode parentNode, String relationName, ArrayList<Token> tokenAnnotations, Sentence sentenceAnno) throws AbstractNodeCasException, ParserRunException {			
+		// Taken mostly from de.tudarmstadt.ukp.dkpro.core.stanfordnlp.util.StanfordAnnotator.createDependencyAnnotation()
+		
+		// Known issue in DKPro
+		if (relationName.equalsIgnoreCase("AUX")) {
+			relationName = "AUX0";
+		}
+
+		String dependencyTypeName = DEPPACKAGE + relationName.toUpperCase();
+		Type type = jcas.getTypeSystem().getType(dependencyTypeName);
+        if (type == null) {
+			throw new ParserRunException("Type [" + dependencyTypeName + "] mapped to tag ["
+					+ relationName + "] is not defined in type system");
+        }
+        
+        Token parentTokenAnno = AbstractNodeCASUtils.nodeToToken(tokenAnnotations, parentNode);
+		
+		AnnotationFS anno = jcas.getCas().createAnnotation(type, sentenceAnno.getBegin(), sentenceAnno.getEnd());
+		anno.setStringValue(type.getFeatureByBaseName("DependencyType"), relationName);
+		anno.setFeatureValue(type.getFeatureByBaseName("Governor"), parentTokenAnno);
+		anno.setFeatureValue(type.getFeatureByBaseName("Dependent"), childToken);
+		jcas.addFsToIndexes(anno);
+	}
 }
